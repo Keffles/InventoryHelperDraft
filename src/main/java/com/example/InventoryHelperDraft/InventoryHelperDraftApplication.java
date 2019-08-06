dcd3711840fb498c35c8b8c906ecee8cc6b389d5
Kelsey <kmiller202011@gmail.com>
2019-08-05 21:29:23 -0500

    This is my first big commitment in my project track.  It's all uphill from here.  Lets do this!!!

---
+++ src/main/java/com/example/InventoryHelperDraft/InventoryHelperDraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.InventoryHelperDraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryHelperDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryHelperDraftApplication.class, args);
+	}
+
+}
